1308abc1645e514a9bc7b655dc442c5c27057501
tvviem <trieuvinhviem@gmail.com>
2022-01-18 19:57:14 +0700

    initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<div class="about">
+    <h2>About Us</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt suscipit,
+        adipisci ipsum vitae quia eos assumenda neque voluptate dignissimos,
+        porro deserunt vel repellendus, saepe laboriosam placeat nesciunt
+        excepturi veritatis laborum.
+    </p>
+    <a href="/guides">View guides</a>
+    <a href="/">HOME</a>
+</div>
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<div class="index">
+    <h2>WELCOMING !!!</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nostrum
+        laudantium dolore voluptas animi tenetur earum vero, mollitia
+        necessitatibus facilis sunt repellendus cum voluptate temporibus
+        nesciunt obcaecati magni aliquam voluptatum!
+    </p>
+    <a href="/guides">View guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
